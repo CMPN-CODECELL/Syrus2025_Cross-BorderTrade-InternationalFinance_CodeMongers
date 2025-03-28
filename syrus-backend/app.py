0bb153227a2a2f8c3cb3c9bab8bb951057d04af2
Prathamesh Shetty <shettyprathamesh13@gmail.com>
2025-03-28 10:45:08 +0530

    Flask basic backend upload

---
+++ syrus-backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+# from routes.trade import trade_blueprint
+# from routes.compliance import compliance_blueprint
+
+app = Flask(__name__)
+CORS(app)
+
+app.get("/")
+def home():
+    return "Syrus Backend Default Route"
+# app.register_blueprint(trade_blueprint, url_prefix="/trade")
+# app.register_blueprint(compliance_blueprint, url_prefix="/compliance")
+
+if __name__ == "__main__":
+    app.run(debug=True)
